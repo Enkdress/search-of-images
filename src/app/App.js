7b70807f2d78b12e82f67a6f651a94a4b57ecd4e
Sergio Correa <felizjanuk12@gmail.com>
2021-02-08 23:43:39 -0500

    Refactor from class component to stateless component

--- src/app/App.js
+++ src/app/App.js
@@ -1,58 +1,37 @@
-import React, {Component} from 'react';
-
+import React, {useState} from 'react';
 import Research from './components/reasearches/Research';
 import Result from './components/results/Results';
-
 import './App.css';
 
-//KEY 13055488-7e28d181f3875eee86a83eee3
+const App = () => {
+  const [hits, setHits] = useState()
 
-
-
-export default class App extends Component{
-
-  state = {
-    request: [],
-    noHits: ""
-  }
-
-  getData = async e => {
+  const getData = async (e) => {
     e.preventDefault()
     const REQ = e.target.research.value;
     const KEY = "13055488-7e28d181f3875eee86a83eee3"
     const URL = `https://pixabay.com/api/?key=${KEY}&q=${REQ}&page=10&per_page=10&image_type=ilustartor&min_width=460px&editors_choice=true`
-    console.log(URL)
 
-    
     const RES = await fetch(URL);
-
-    //HERE ARE THE ARRAY WITH THE REQUEST 
     const data = await RES.json();
-    
-    this.setState( { request: data.hits } )
-
+    setHits(data.hits)
   }
 
-  render(){
-
-    
-    return (
-      
-        <div className="container">
-          <div className="research-cont">
-            <Research getData = {this.getData} />           
-          </div>
-          <div className="result row">
-            <div className="card-container">
-              <Result data = {this.state.request} />
-            </div>
-          </div>
-          <br id = "endPage"/>
+  return (
+    <div className="container">
+      <div className="research-cont">
+        <Research getData={getData} />
+      </div>
+      <div className="result row">
+        <div className="card-container">
+          <Result data={hits} />
         </div>
+      </div>
+      <br id="endPage" />
+    </div>
+  )
 
-    )
-  }
 }
 
-
+export default App;
 
